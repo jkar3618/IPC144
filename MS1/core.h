e9544a079a84190043ffefcdac60f07f074c5f5d
jkar3618 <113157603+jkar3618@users.noreply.github.com>
2023-04-05 15:49:25 -0400

    Add files via upload

---
+++ MS1/core.h
@@ -0,0 +1,24 @@
+// Tester Function Definitions
+
+// Function for Input Integer
+int inputInt(void);
+
+// Function for Input Positive Integer
+int inputIntPositive(void);
+
+// Range of Integer
+int inputIntRange(int min, int max);
+
+// Function for Input Character
+char inputCharOption(char str[]);
+
+// Function for Input Character String
+void inputCString(char* cStringValue, int min, int max);
+
+void displayFormattedPhone(const char* phoneNum);
+
+// Clear the standard input buffer
+void clearInputBuffer(void);
+
+// Wait for user to input the "enter" key to continue
+void suspend(void);
